b1d4918e1e28bbe80297582cc5225a4e865f561a
Tometchy <tometchy@gmail.com>
2020-02-10 12:24:31 +0100

    Compose client with server1

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading;
 
 namespace Client
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var httpClient = new HttpClient();
+            while (true)
+            {
+                Thread.Sleep(5000);
+                var result = httpClient.GetAsync("http://server1:5000/api/values").Result;
+                Console.WriteLine(result);
+                Console.WriteLine(result.Content.ReadAsStringAsync().Result);
+            }
         }
     }
 }
