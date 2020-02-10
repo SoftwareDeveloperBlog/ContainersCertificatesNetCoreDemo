dfa6dbe7c331f7920df407a45605d77b80cff970
Tometchy <tometchy@gmail.com>
2020-02-10 14:01:29 +0100

    Connect client to server1 via ssl without addding server cert
    Server cert is not added to client machine, so currently it is correct behaviour,
    that https connections doesn't work.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Threading;
 
@@ -11,11 +12,44 @@ namespace Client
             var httpClient = new HttpClient();
             while (true)
             {
-                Thread.Sleep(5000);
-                var result = httpClient.GetAsync("http://server1:5000/api/values").Result;
-                Console.WriteLine(result);
-                Console.WriteLine(result.Content.ReadAsStringAsync().Result);
+                try
+                {
+                    Thread.Sleep(5000);
+                    var result = httpClient.GetAsync("https://server1:5001/api/values").Result;
+                    Console.WriteLine(result);
+                    Console.WriteLine(result.Content.ReadAsStringAsync().Result);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Could not connect. Openssl diagnosis:");
+                    Console.WriteLine("openssl s_client -connect server1:5001 -debug -state".Bash());
+                    throw;
+                }
             }
+        }
+    }
+
+    public static class ShellHelper
+    {
+        public static string Bash(this string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
         }
     }
 }
